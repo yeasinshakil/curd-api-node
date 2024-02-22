518939b566b5d0414d79e393e6df45826a1ad947
yeasinshakil <shakilrock1998@gmail.com>
2024-02-22 15:58:44 +0600

    product data saved successfully to the database

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const { default: mongoose } = require('mongoose')
+const Product = require("./models/producs.model")
 const dotenv = require('dotenv')
 
 dotenv.config()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello from code')
 })
 
+app.post('/api/product', async (req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 mongoose.connect(process.env.MONGO_CONNECTION_URI).then(() => {
     console.log('connent to the database');
