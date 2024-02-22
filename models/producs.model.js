ffab0a477ac287c080340c2e3f2976bed871524a
yeasinshakil <shakilrock1998@gmail.com>
2024-02-22 15:24:45 +0600

    product model schema

---
+++ models/producs.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'product name required!']
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    image: {
+        type: String,
+        required: false
+    }
+}, { timestamps: true })
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
